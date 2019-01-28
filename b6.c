6d1cbde60cab7e132516d6b3da7f3d75c043b37b
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-01-28 16:10:26 +0530

    Create b6.c

---
+++ b6.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a;
+    printf("enter the year");
+    scanf("%d",&a);
+    if(a%4==0 && a%100!=0||a%400==0)
+    {
+  
+    printf("leap year");
+    }
+    else
+    {
+        printf("not a leap year");
+    }
+    getch();
+}

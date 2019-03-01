1b49595ed2a5e0d99933515b1aa2a6e88b131ebe
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-01 11:11:48 +0530

    Update B13.c

--- B13.c
+++ B13.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int m, i, flag = 0;
 printf("Enter a positive integer: ");
-    scanf("%d", &n);
-for(i = 2; i <= n/2; ++i)
+    scanf("%d", &m);
+for(i = 2; i <= m/2; ++i)
     {
-        if(n%i == 0)
+        if(m%i == 0)
         {
             flag = 1;
             break;
         }
     }
-    if (n == 1) 
+    if (m == 1) 
     {
       printf("1 is neither a prime nor a composite number.");
     }
     else 
     {
         if (flag == 0)
-          printf("%d is a prime number.", n);
+          printf("%d is a prime number.", m);
         else
-          printf("%d is not a prime number.", n);
+          printf("%d is not a prime number.", m);
     }
     
     return 0;

5ac1cb085c7b2d170ea45eb8bd8089979b907d26
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-01 11:38:25 +0530

    Create b27.c

---
+++ b27.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+  int a;
+   scanf("%d",&a);
+   if(a<0 || a>0)
+   {
+     printf("Yes");
+   }
+   else
+   {
+     printf("No");
+   }
+  
+     return 0;
+}

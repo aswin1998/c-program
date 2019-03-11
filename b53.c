059186666b0e089493876ab2e58ac30cac34a195
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-11 21:20:40 +0530

    Create b53.c

---
+++ b53.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) 
+{
+  int n,s=0,m;
+scanf("%d",&n);
+while(n>0)
+{
+  m=n%10;
+  s=s+r;
+  n=n/10;
+}
+printf("%d",s);
+
+return 0;
+}

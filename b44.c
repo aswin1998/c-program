5c425ab926fff8af72b42f6df2d033f4c1b8ddcb
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-11 20:41:48 +0530

    Create b44.c

---
+++ b44.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int m;
+   scanf("%d",&m);
+   
+   if(m>=1 && m<=10)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+ 
+  return 0;
+}

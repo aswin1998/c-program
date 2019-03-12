8ab642ccfabec0a20a04a004f0b3d24f0324c3d7
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-12 21:12:36 +0530

    Create b68.c

---
+++ b68.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int d;
+    scanf("%d",&d);
+    
+    if(d%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+return 0;
+}

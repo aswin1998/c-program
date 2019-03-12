1a8ff9a2693349040562a96a0ac9c2debff989e6
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-12 21:16:53 +0530

    Create b69.c

---
+++ b69.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,x;
+    scanf("%d %d",&a,&b);
+    x=a-b;
+    if(x%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+return 0;
+}

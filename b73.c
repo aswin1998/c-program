6db3854290feef1cc6a55dd4f67b9a0eacdce997
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-14 20:42:56 +0530

    Create b73.c

---
+++ b73.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int N,L,R,a=0,i;
+    scanf("%d",&N);
+    scanf("%d %d",&L,&R);
+    for(i=L+1;i<R;i++)
+{
+if(i==N)
+{
+    a=1;
+break;
+}
+}
+if (a==1) {
+        printf("yes");
+    }    
+    else {
+        printf("no");
+    }
+    return 0;
+}

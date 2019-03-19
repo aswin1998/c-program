a7a4badd5dd2f918ec07ce60332b84471e03dafa
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-19 16:36:01 +0530

    Create p10.c

---
+++ p10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+  char str[10000],tot[10000];
+  int i,km=0;
+  gets(str);
+  gets(tor);
+  for(i=0;str[i]!='\0';i++)
+  {  
+          if(str[i]!=tot[i])
+          {
+              
+             km++; 
+           
+
+          }
+   
+      }
+  
+  if(km==1)
+  printf("yes");
+  else
+  printf("no");
+    return 0;
+}

4c343a91d76697c92e4536979c379b5a83d9515c
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-25 15:52:53 +0530

    Update p22.c

--- p22.c
+++ p22.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main()
+{
+
+ int Num,K,max,GR=1,i;
+    scanf("%d %d",&Num,&K);
+    max = ((Num<K) ? Num : K);
+
+    for(i=1; i<=max; i++)
+    {
+    
+        if(Num%i==0 && K%i==0)
+        {
+            GR = i;
+        }
+    }
+
+    printf("%d",GR);
+
+return 0;
+}

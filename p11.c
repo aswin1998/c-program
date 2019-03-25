c8796276e9829bd5d77e52a30a664071684aac25
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-25 10:10:07 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h> 
+
+int main()
+{
+    char sa[20];
+    scanf("%s",sa);
+  if(sa[0]=='s' || sa[0]=='S')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+ 
+    return 0;
+}

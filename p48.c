d4fd424817bc2e469d71d13f3614c9b29c1f5084
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-26 16:06:46 +0530

    Create p48.c

---
+++ p48.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num,i,count=0;
+	scanf("%d",&num);
+    for(i=2;i<num;i++)
+    {
+        if(i%2==1 && n%umi==0)
+        {
+            printf("%d",i);
+            
+        }
+    }
+	return 0;
+}

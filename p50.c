586f5b7296eb63f1d9cf015a56976a742a47e040
aswin1998 <36395955+aswin1998@users.noreply.github.com>
2019-03-26 16:09:04 +0530

    Rename p40.c to p50.c

---
+++ p50.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+
+int N;
+scanf("%d",&Num);
+
+if(Num>=-32,768 && Num<= 32,767  || Num>=-2,147,483,648 && N<=2,147,483,647)
+{
+    printf("INT");
+}
+else
+{
+    printf("LONG");
+}
+
+
+    return 0;
+}
